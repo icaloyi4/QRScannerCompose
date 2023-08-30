d48e6e46290eb13e950fb5ac702f874410d3a383
Ical Risaldi <r.harisrisaldi@gmail.com>
2023-08-30 13:33:35 +0700

    Fix Pending intent

--- app/src/main/java/co/id/mii/qrscanner/features/notification/viewmodel/NotificationViewModel.kt
+++ app/src/main/java/co/id/mii/qrscanner/features/notification/viewmodel/NotificationViewModel.kt
@@ -1,27 +1,21 @@
 package co.id.mii.qrscanner.features.notification.viewmodel
 
-import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.content.ContextWrapper
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
-import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
 import androidx.core.net.toUri
 import androidx.lifecycle.ViewModel
 import co.id.mii.qrscanner.MainActivity
 import co.id.mii.qrscanner.R
-import co.id.mii.qrscanner.core.notification.NotificationService
 import co.id.mii.qrscanner.core.other.dummyPayloadString
 import co.id.mii.qrscanner.features.notification.model.PayloadNotificationModel
 import co.id.mii.qrscanner.features.notification.repository.NotificationRepository
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import java.util.Random
 
@@ -33,11 +27,19 @@ class NotificationViewModel(val repo : NotificationRepository) : ViewModel() {
         val intent = Intent(Intent.ACTION_VIEW, payload.message?.data?.deepLink?.toUri(), context, MainActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
-
-         val pending: PendingIntent? = TaskStackBuilder.create(context).run {
-             addNextIntentWithParentStack(intent)
-             getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+         val pending: PendingIntent? =
+         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+             TaskStackBuilder.create(context).run {
+                 addNextIntentWithParentStack(intent)
+                 getPendingIntent(0, PendingIntent.FLAG_MUTABLE)
+             }
+         } else {
+             TaskStackBuilder.create(context).run {
+                 addNextIntentWithParentStack(intent)
+                 getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+             }
          }
+
 
 
          val channelId = context.getString(R.string.default_notification_channel_id)
